cf655228bc54e2a4f8b5ac5380683592996bf8c0
kimquangtran <kimquangtran185@gmail.com>
2020-11-18 14:46:54 +0700

    add covid, tag, change name

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,61 +14,63 @@ $(function () {
 
     var $area = $('area');
 
-    $('map').imageMapResize();
-    $(window).on('load', function (e) {
-        const wWidth = $(window).width();
-        if (wWidth > 768) {
-            $('#image-map').maphilight({
-                stroke: true,
-                strokeColor: 'ffffff',
-                fill: true,
-                fillColor: 'DC000B',
-                fillOpacity: 1,
-            });
-            // $area
-            $('.area')
-                .mouseover(function () {
-                    $area.filter('[data-area="' + $(this).data('area') + '"]').trigger('mouseover');
-                    $(this).removeClass('hover');
-                })
-                .mouseout(function () {
+    if ($area[0]) {
+        $('map').imageMapResize();
+        $(window).on('load', function (e) {
+            const wWidth = $(window).width();
+            if (wWidth > 768) {
+                $('#image-map').maphilight({
+                    stroke: true,
+                    strokeColor: 'ffffff',
+                    fill: true,
+                    fillColor: 'DC000B',
+                    fillOpacity: 1,
+                });
+                // $area
+                $('.area')
+                    .mouseover(function () {
+                        $area.filter('[data-area="' + $(this).data('area') + '"]').trigger('mouseover');
+                        $(this).removeClass('hover');
+                    })
+                    .mouseout(function () {
+                        $area.trigger('mouseout');
+                    });
+                $area
+                    .mouseenter(function () {
+                        $('.area')
+                            .filter('[data-area="' + $(this).data('area') + '"]')
+                            .addClass('hover');
+                    })
+                    .mouseleave(function () {
+                        $('.area').removeClass('hover');
+                    });
+
+                $('.area, area').click(function () {
                     $area.trigger('mouseout');
                 });
-            $area
-                .mouseenter(function () {
-                    $('.area')
-                        .filter('[data-area="' + $(this).data('area') + '"]')
-                        .addClass('hover');
-                })
-                .mouseleave(function () {
-                    $('.area').removeClass('hover');
-                });
-
-            $('.area, area').click(function () {
-                $area.trigger('mouseout');
-            });
-        } else {
-            // $('#image-map-sp').maphilight({
-            //     stroke: true,
-            //     strokeColor: 'ffffff',
-            //     fill: true,
-            //     fillColor: 'DC000B',
-            //     fillOpacity: 1,
-            // });
-            // $('.area').click(function () {
-            //     var $this = $(this);
-            //     $area.trigger('mouseout');
-            //     if ($this.hasClass('active')) {
-            //         $(this).removeClass('active');
-            //         $area.filter('[data-area="' + $(this).data('area') + '"]').trigger('mouseout');
-            //     } else {
-            //         $('.area').removeClass('active');
-            //         $(this).addClass('active');
-            //         $area.filter('[data-area="' + $(this).data('area') + '"]').trigger('mouseover');
-            //     }
-            // });
-        }
-    });
+            } else {
+                // $('#image-map-sp').maphilight({
+                //     stroke: true,
+                //     strokeColor: 'ffffff',
+                //     fill: true,
+                //     fillColor: 'DC000B',
+                //     fillOpacity: 1,
+                // });
+                // $('.area').click(function () {
+                //     var $this = $(this);
+                //     $area.trigger('mouseout');
+                //     if ($this.hasClass('active')) {
+                //         $(this).removeClass('active');
+                //         $area.filter('[data-area="' + $(this).data('area') + '"]').trigger('mouseout');
+                //     } else {
+                //         $('.area').removeClass('active');
+                //         $(this).addClass('active');
+                //         $area.filter('[data-area="' + $(this).data('area') + '"]').trigger('mouseover');
+                //     }
+                // });
+            }
+        });
+    }
 
     $('.accordion-toggle').click(function (e) {
         var $this = $(this);
